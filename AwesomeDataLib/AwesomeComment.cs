82c5eeca8f6b013879556eff2bb7fc8f804bc6f5
Elliot Franford <efranford@roamdata.com>
2013-04-08 18:25:23 -0400

    Updated to work
    Well that was a rocky start

---
+++ AwesomeDataLib/AwesomeComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeDataLib
+{
+    public class AwesomeComment
+    {
+        [Key]
+        public virtual long AwesomeCommentId { get; set; }
+        [MaxLength(2000)]
+        public virtual string CommentText { get; set; }
+
+        [ForeignKey("ResponseToCommentId")]
+        public virtual AwesomeComment ResponseToComment { get; set; }
+        public virtual long? ResponseToCommentId { get; set; }
+    }
+}
---
+++ AwesomeDataLib/AwesomeThing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeDataLib
+{
+    public class AwesomeThing
+    {
+        [Key]
+        public virtual long AwesomeThingId { get; set; }
+        [MaxLength(200)]
+        public virtual string Name { get; set; }
+        [MaxLength(1000)]
+        public virtual string Description { get; set; }
+        [MaxLength(500)]
+        public virtual string URL { get; set; }
+        public virtual long Rating { get; set; }
+        public virtual long PositiveVotes { get; set; }
+        public virtual long NegativeVotes { get; set; }
+    }
+}
